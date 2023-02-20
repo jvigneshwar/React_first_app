65faafccb43a589c3458644f2d69629f7296e44f
jvigneshwar <77946769+jvigneshwar@users.noreply.github.com>
2023-02-20 13:41:06 +0530

    Add files via upload

---
+++ src/components/CompanyLogo.js
@@ -0,0 +1,7 @@
+const Companylogo = () => {
+    return(
+        <img src="https://electronicsclap.com/wp-content/uploads/2022/05/pTron-New-Logo-Emblem.jpg"  id="Companylogo"/>
+    );
+}
+
+export default Companylogo;
---
+++ src/components/ProductPrice.js
@@ -0,0 +1,11 @@
+import Button from "./Button";
+const ProdectPrice = () => {
+    return(
+        <div id="pb">
+        <h2 id="ProdectPrice">$999</h2>
+        <Button/>
+        </div>
+    );
+}
+
+export default ProdectPrice;
